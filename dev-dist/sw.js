23730e0b4f7d4c1c50ae229bb5c6a97dd133464e
Loic Berthod <lberthod@gmail.com>
2025-05-21 10:06:28 +0200

    affectation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.evkk7jl779g"
+    "revision": "0.vrq28du2fh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/Dashboard/DashboardForms/AffectationStageEtudiant.vue
+++ src/components/Dashboard/DashboardForms/AffectationStageEtudiant.vue
@@ -2,65 +2,181 @@
   <Navbar />
   <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
     <section class="text-white text-center py-5 rounded-lg">
-      <h1 class="text-4xl font-bold mb-4">Affecter un stage à un étudiant</h1>
+      <h1 class="text-4xl font-bold mb-4">Affecter un stage à un étudiant (SBA23)</h1>
     </section>
     <section class="mt-5">
-      <div class="card p-4 max-w-2xl mx-auto">
-        <form @submit.prevent="affecterStage">
-          <div class="field mb-4">
-            <label for="etudiant" class="block mb-2">Étudiant</label>
-            <Dropdown id="etudiant" v-model="selectedEtudiant" :options="etudiantsOptions" optionLabel="nom" optionValue="id" class="w-full" placeholder="Sélectionnez un étudiant" filter filterPlaceholder="Rechercher un étudiant..."/>
-          </div>
-          <div class="field mb-4">
-            <label for="stage" class="block mb-2">Stage(s)</label>
-            <MultiSelect id="stage" v-model="selectedStages" :options="stagesOptions" optionLabel="titre" optionValue="id" class="w-full" placeholder="Sélectionnez un ou plusieurs stages" filter filterPlaceholder="Rechercher un stage..."/>
-          </div>
-          <div class="flex justify-content-end">
-            <Button type="submit" label="Affecter" class="p-button-success" :disabled="!selectedEtudiant || selectedStages.length === 0" />
-          </div>
-        </form>
+      <div class="card p-4 max-w-4xl mx-auto">
+        <table class="w-full">
+          <thead>
+            <tr>
+              <th>Nom et Prénom</th>
+              <th>PFP1</th>
+              <th>LESE</th>
+              <th>SAE</th>
+              <th>CAS</th>
+              <th>PFP2</th>
+              <th>LESE</th>
+              <th>SAE</th>
+              <th>CAS</th>
+              <th>Affecter</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="etudiant in etudiantsSBA23" :key="etudiant.id">
+              <td>{{ etudiant.nom }}</td>
+              <td>
+                <Dropdown
+  v-model="affectations[etudiant.id]"
+  :options="stagesOptions"
+  optionLabel="titre"
+  optionValue="id"
+  placeholder="Sélectionnez PFP1"
+  class="w-full"
+  filter
+  filterPlaceholder="Rechercher un stage..."
+/>
+              </td>
+              <td>
+                <div style="display: flex; flex-direction: column; align-items: center;">
+  <Checkbox :binary="true" :inputId="'lese-' + etudiant.id" :modelValue="!!((conditionsPFP1[etudiant.id] || {}).lese)" @change="updateCondition('PFP1', etudiant.id, 'lese', $event.target.checked)" />
+  <span style="font-size: 0.8em; margin-top: 2px;"></span>
+</div>
+              </td>
+              <td>
+                <div style="display: flex; flex-direction: column; align-items: center;">
+  <Checkbox :binary="true" :inputId="'sae-' + etudiant.id" :modelValue="!!((conditionsPFP1[etudiant.id] || {}).sae)" @change="updateCondition('PFP1', etudiant.id, 'sae', $event.target.checked)" />
+  <span style="font-size: 0.8em; margin-top: 2px;"></span>
+</div>
+              </td>
+              <td>
+                <div style="display: flex; flex-direction: column; align-items: center;">
+  <Checkbox :binary="true" :inputId="'cas-' + etudiant.id" :modelValue="!!((conditionsPFP1[etudiant.id] || {}).cas)" @change="updateCondition('PFP1', etudiant.id, 'cas', $event.target.checked)" />
+  <span style="font-size: 0.8em; margin-top: 2px;"></span>
+</div>
+              </td>
+              <td>
+                <Dropdown
+                  v-model="affectationsPFP2[etudiant.id]"
+                  :options="stagesOptions"
+                  optionLabel="titre"
+                  optionValue="id"
+                  placeholder="Sélectionnez  PFP2"
+                  class="w-full"
+                  filter
+                  filterPlaceholder="Rechercher un stage..."
+                />
+              </td>
+              <td>
+                <div style="display: flex; justify-content: center; align-items: center; min-width:32px;">
+  <Checkbox :binary="true" :inputId="'lese2-' + etudiant.id" :modelValue="!!((conditionsPFP2[etudiant.id] || {}).lese)" @change="updateCondition('PFP2', etudiant.id, 'lese', $event.target.checked)" style="transform: scale(0.9);" />
+</div>
+              </td>
+              <td>
+                <div style="display: flex; justify-content: center; align-items: center; min-width:32px;">
+  <Checkbox :binary="true" :inputId="'sae2-' + etudiant.id" :modelValue="!!((conditionsPFP2[etudiant.id] || {}).sae)" @change="updateCondition('PFP2', etudiant.id, 'sae', $event.target.checked)" style="transform: scale(0.9);" />
+</div>
+              </td>
+              <td>
+                <div style="display: flex; justify-content: center; align-items: center; min-width:32px;">
+  <Checkbox :binary="true" :inputId="'cas2-' + etudiant.id" :modelValue="!!((conditionsPFP2[etudiant.id] || {}).cas)" @change="updateCondition('PFP2', etudiant.id, 'cas', $event.target.checked)" style="transform: scale(0.9);" />
+</div>
+              </td>
+              <td>
+                <Button
+                  label="Affecter"
+                  class="p-button-success"
+                  :disabled="!affectations[etudiant.id]"
+                  @click="affecterStage(etudiant.id)"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <div v-if="message" class="mt-4 text-green-600">{{ message }}</div>
       </div>
     </section>
   </div>
 </template>
 
+
 <script>
 import Navbar from '@/components/Utils/Navbar.vue';
-import Dropdown from 'primevue/dropdown';
-import MultiSelect from 'primevue/multiselect';
+import InputText from 'primevue/inputtext';
+import Checkbox from 'primevue/checkbox';
 import Button from 'primevue/button';
-import Divider from 'primevue/divider';
-import { db } from '../../../../firebase.js';
-import { ref, onValue, update } from "firebase/database";
+import { db, getUserNameById } from 'root/firebase.js';
+import { ref, onValue, update } from 'firebase/database';
 
 export default {
   name: 'AffectationStageEtudiant',
-  components: { Navbar, Dropdown, MultiSelect, Button, Divider },
+  components: { Navbar, InputText, Button, Checkbox },
   data() {
     return {
-      etudiantsOptions: [], // [{ id, nom }]
-      stagesOptions: [],    // [{ id, titre }]
-      stagesRaw: {},         // Données brutes des places
-      selectedEtudiant: null,
-      selectedStages: [],
+      etudiantsSBA23: [], // Will be filled from Firebase
+      affectations: {},   // Pour PFP1
+      affectationsPFP2: {}, // Pour PFP2
+      conditionsPFP1: {}, // { [studentId]: { lese, sae, cas } }
+      conditionsPFP2: {}, // { [studentId]: { lese, sae, cas } }
+      stagesOptions: [], // Pour dropdown ou multiselect
+      stagesRaw: {},     // Pour accès brut si besoin
       message: '',
     };
   },
-  mounted() {
-    // Charger les étudiants (IDs)
+  async mounted() {
+    // Charger dynamiquement les étudiants BA23
     onValue(ref(db, 'Students'), (snapshot) => {
       const studentsData = snapshot.val() || {};
-      onValue(ref(db, 'Users'), (usersSnap) => {
+      onValue(ref(db, 'Users'), async (usersSnap) => {
         const usersData = usersSnap.val() || {};
-        this.etudiantsOptions = Object.entries(studentsData).map(([id, val]) => {
-          const user = usersData[id];
-          let label = id;
-          if (user && user.Nom && user.Prenom) label = user.Nom + ' ' + user.Prenom;
-          else if (user && user.Nom) label = user.Nom;
-          else if (user && user.Prenom) label = user.Prenom;
-          return { id, nom: label };
+        // Filtrer BA23 et construire nom complet
+        const ba23 = await Promise.all(Object.entries(studentsData)
+          .filter(([id, val]) => val && (val.Classe === 'BA23' || val.Class === 'BA23'))
+          .map(async ([id]) => {
+            let nom = '';
+            const user = usersData[id];
+            if (user && user.Nom && user.Prenom) nom = user.Nom + ' ' + user.Prenom;
+            else if (user && user.Nom) nom = user.Nom;
+            else if (user && user.Prenom) nom = user.Prenom;
+            else nom = id;
+            return { id, nom };
+          })
+        );
+        this.etudiantsSBA23 = ba23;
+
+        // --- Récupération des affectations et conditions déjà enregistrées ---
+        const affectations = {};
+        const affectationsPFP2 = {};
+        const conditionsPFP1 = {};
+        const conditionsPFP2 = {};
+        Object.entries(studentsData).forEach(([id, val]) => {
+          if (val && Array.isArray(val.PFP_valided)) {
+            // PFP1
+            if (val.PFP_valided[0]) {
+              const pfp1 = val.PFP_valided[0];
+              if (pfp1.ID_Place) affectations[id] = pfp1.ID_Place;
+              conditionsPFP1[id] = {
+                lese: !!pfp1.LESE,
+                sae: !!pfp1.SAE,
+                cas: !!pfp1.CAS
+              };
+            }
+            // PFP2
+            if (val.PFP_valided[1]) {
+              const pfp2 = val.PFP_valided[1];
+              if (pfp2.ID_Place) affectationsPFP2[id] = pfp2.ID_Place;
+              conditionsPFP2[id] = {
+                lese: !!pfp2.LESE,
+                sae: !!pfp2.SAE,
+                cas: !!pfp2.CAS
+              };
+            }
+          }
         });
+        this.affectations = affectations;
+        this.affectationsPFP2 = affectationsPFP2;
+        this.conditionsPFP1 = conditionsPFP1;
+        this.conditionsPFP2 = conditionsPFP2;
+        // --- FIN récupération ---
       });
     });
     // Charger les places de stage avec infos d'institution
@@ -88,59 +204,112 @@ export default {
             if (institutionName) labelParts.push(institutionName);
             if (titrePlace) labelParts.push(titrePlace);
             if (nomPlace && nomPlace !== titrePlace) labelParts.push(nomPlace);
-            let titre = labelParts.join(' – ') + critLabel;
+            let titre = labelParts.join(' – ') ;
             return { id, titre };
           });
       });
     });
   },
   methods: {
-    async affecterStage() {
-      if (!this.selectedEtudiant || this.selectedStages.length === 0) return;
+    updateCondition(pfp, id, field, value) {
+      if (pfp === 'PFP1') {
+        const prev = this.conditionsPFP1[id] || {};
+        this.conditionsPFP1 = {
+          ...this.conditionsPFP1,
+          [id]: { ...prev, [field]: value }
+        };
+      } else if (pfp === 'PFP2') {
+        const prev = this.conditionsPFP2[id] || {};
+        this.conditionsPFP2 = {
+          ...this.conditionsPFP2,
+          [id]: { ...prev, [field]: value }
+        };
+      }
+    },
+    async affecterStage(etudiantId) {
+      // PFP1
+      const stageId1 = this.affectations[etudiantId];
+      // PFP2
+      const stageId2 = this.affectationsPFP2[etudiantId];
+
+      // Construction objet PFP1
+      let affectationData1 = null;
+      if (stageId1 && this.stagesRaw[stageId1]) {
+        const stageData1 = this.stagesRaw[stageId1];
+        const conds1 = this.conditionsPFP1[etudiantId] || {};
+        affectationData1 = {
+          AIGU: stageData1.AIGU === true || stageData1.AIGU === 'true',
+          AMBU: stageData1.AMBU === true || stageData1.AMBU === 'true',
+          DE: stageData1.DE === true || stageData1.DE === 'true',
+          Domaine: stageData1.Domaine || stageData1.NomPlace || '',
+          FR: stageData1.FR === true || stageData1.FR === 'true',
+          ID_PFP: stageData1.IDPlace || stageData1.InstitutionId || '',
+          ID_Place: stageId1,
+          MSQ: stageData1.MSQ === true || stageData1.MSQ === 'true',
+          NEUROGER: stageData1.NEUROGER === true || stageData1.NEUROGER === 'true',
+          REHAB: stageData1.REHAB === true || stageData1.REHAB === 'true',
+          SYSINT: stageData1.SYSINT === true || stageData1.SYSINT === 'true',
+          LESE: !!conds1.lese,
+          SAE: !!conds1.sae,
+          CAS: !!conds1.cas
+        };
+      }
+      // Construction objet PFP2 (identique à PFP1)
+      let affectationData2 = null;
+      if (stageId2 && this.stagesRaw[stageId2]) {
+        const stageData2 = this.stagesRaw[stageId2];
+        const conds2 = this.conditionsPFP2[etudiantId] || {};
+        affectationData2 = {
+          AIGU: stageData2.AIGU === true || stageData2.AIGU === 'true',
+          AMBU: stageData2.AMBU === true || stageData2.AMBU === 'true',
+          DE: stageData2.DE === true || stageData2.DE === 'true',
+          Domaine: stageData2.Domaine || stageData2.NomPlace || '',
+          FR: stageData2.FR === true || stageData2.FR === 'true',
+          ID_PFP: stageData2.IDPlace || stageData2.InstitutionId || '',
+          ID_Place: stageId2,
+          MSQ: stageData2.MSQ === true || stageData2.MSQ === 'true',
+          NEUROGER: stageData2.NEUROGER === true || stageData2.NEUROGER === 'true',
+          REHAB: stageData2.REHAB === true || stageData2.REHAB === 'true',
+          SYSINT: stageData2.SYSINT === true || stageData2.SYSINT === 'true',
+          LESE: !!conds2.lese,
+          SAE: !!conds2.sae,
+          CAS: !!conds2.cas
+        };
+      }
+      // Construire le tableau pour PFP_valided
+      const pfpValidedArray = [];
+      if (affectationData1) pfpValidedArray[0] = affectationData1;
+      if (affectationData2) pfpValidedArray[1] = affectationData2;
       try {
-        const pfp2 = [];
-        const PFP_valided = [];
-        for (const stageId of this.selectedStages) {
-          const place = this.stagesOptions.find(opt => opt.id === stageId);
-          const fullPlace = this.stagesRaw && this.stagesRaw[stageId] ? this.stagesRaw[stageId] : null;
-          if (!place) continue;
-          pfp2.push({
-            id: stageId,
-            nom: place.titre || ''
-          });
-          if (fullPlace) {
-            const obj = {};
-            obj.Domaine = fullPlace.Domaine || fullPlace.Titre || '';
-            obj.ID_PFP = stageId;
-            obj.MSQ = !!fullPlace.MSQ;
-            obj.NEUROGER = !!fullPlace.NEUROGER;
-            obj.REHAB = !!fullPlace.REHAB;
-            obj.AMBU = !!fullPlace.AMBU;
-            obj.FR = !!fullPlace.FR;
-            obj.AIGU = !!fullPlace.AIGU;
-            obj.DE = !!fullPlace.DE;
-            obj.SYSINT = !!fullPlace.SYSINT;
-            PFP_valided.push(obj);
-          }
-        }
-        // Met à jour PFP_2 à la racine
-        await update(ref(db, `Students/${this.selectedEtudiant}`), {
-          'PFP_valided/1': pfp2,
+        await update(ref(db, `Students/${etudiantId}`), {
+          'PFP_valided': pfpValidedArray
         });
-        // Met à jour PFP_valided dans le sous-noeud '1'
-        await update(ref(db, `Students/${this.selectedEtudiant}`), {
-          'PFP_valided/1': PFP_valided[0]
-        });
-        this.message = "Stage(s) affecté(s) avec succès !";
+        this.message = `Affectations mises à jour pour l'étudiant ${etudiantId}`;
       } catch (e) {
         this.message = "Erreur lors de l'affectation.";
       }
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
+.card {
+  background: #fff;
+  border-radius: 1rem;
+  box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+}
+th, td {
+  padding: 0.75rem;
+  border-bottom: 1px solid #e5e7eb;
+  text-align: left;
+}
+th {
+  background: #f3f4f6;
+  color:black;
+
+}
+
 .card {
   background: var(--surface-card);
   border-radius: 12px;
