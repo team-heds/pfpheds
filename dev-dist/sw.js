a4e0b6ce6d9f680632f3bc07292292435532e291
Loic Berthod <lberthod@gmail.com>
2025-06-11 16:37:28 +0200

    votationNormal

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.045ueht52a"
+    "revision": "0.b28up57t1a"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
