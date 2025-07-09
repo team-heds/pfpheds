f3fd7d87d3d79ddaa93b6d625b44def079f0862e
Antoine Quarroz <51474853+antoinequarroz@users.noreply.github.com>
2025-07-09 15:50:47 +0200

    Heds25 257 docker (#72)
    * docker
    
    * port back
    
    ---------
    
    Co-authored-by: AntoineQuarroz <antoine.quarroz@hes-so.ch>

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/api/ping', (req, res) => {
+  res.send('pong')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Backend OK sur le port ${PORT}`))
