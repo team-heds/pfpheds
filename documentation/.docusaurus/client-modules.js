0ce8b18788e9ce362e33c71af9ae50ad87fb6767
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2025-11-06 09:45:56 +0100

    tst

--- documentation/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\antoine.quarroz\\Desktop\\LabDev\\pfpheds\\documentation\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\antoine.quarroz\\Desktop\\LabDev\\pfpheds\\documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\antoine.quarroz\\Desktop\\LabDev\\pfpheds\\documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\antoine.quarroz\\Desktop\\LabDev\\pfpheds\\documentation\\src\\css\\custom.css"),
-];
