fa112b10dadce82ffba2d514c1cbc20934f63258
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2025-12-19 13:47:57 +0100

    deploy

---
+++ src/views/admin/TrackDebugView.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <AdminLayout>
+    <template #header>
+      <PageHeader title="Track Debug" subtitle="Outil de diagnostic" icon="pi pi-wrench" />
+    </template>
+
+    <div class="p-4">
+      <Card>
+        <template #content>
+          <div class="text-600">Cette page est en construction.</div>
+        </template>
+      </Card>
+    </div>
+  </AdminLayout>
+</template>
+
+<script setup>
+import AdminLayout from '@/components/admin/layouts/AdminLayout.vue';
+import PageHeader from '@/components/admin/common/PageHeader.vue';
+import Card from 'primevue/card';
+</script>
