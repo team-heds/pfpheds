b41c6eefd83a68fc9d2588a3d631a815444d489d
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2026-07-06 10:24:17 +0200

    mise a jour doc

--- documentation/.docusaurus/routes.js
+++ documentation/.docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/docs/__docusaurus/debug',
+    component: ComponentCreator('/docs/__docusaurus/debug', 'e58'),
+    exact: true
+  },
+  {
+    path: '/docs/__docusaurus/debug/config',
+    component: ComponentCreator('/docs/__docusaurus/debug/config', '2ce'),
+    exact: true
+  },
+  {
+    path: '/docs/__docusaurus/debug/content',
+    component: ComponentCreator('/docs/__docusaurus/debug/content', '11b'),
+    exact: true
+  },
+  {
+    path: '/docs/__docusaurus/debug/globalData',
+    component: ComponentCreator('/docs/__docusaurus/debug/globalData', 'f13'),
+    exact: true
+  },
+  {
+    path: '/docs/__docusaurus/debug/metadata',
+    component: ComponentCreator('/docs/__docusaurus/debug/metadata', 'bff'),
+    exact: true
+  },
+  {
+    path: '/docs/__docusaurus/debug/registry',
+    component: ComponentCreator('/docs/__docusaurus/debug/registry', '830'),
+    exact: true
+  },
+  {
+    path: '/docs/__docusaurus/debug/routes',
+    component: ComponentCreator('/docs/__docusaurus/debug/routes', '13e'),
+    exact: true
+  },
+  {
     path: '/docs/landing',
     component: ComponentCreator('/docs/landing', '80b'),
     exact: true
