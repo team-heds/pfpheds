1d2077bc88748f1c3e37d0a528ad5d6322de0625
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2026-08-06 14:53:09 +0200

    add cas particuliers management panel

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /// <reference lib="webworker" />
 /* eslint-disable no-undef */
-const SW_VERSION = '0.2.8';
+const SW_VERSION = '0.2.9';
 void SW_VERSION;
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
